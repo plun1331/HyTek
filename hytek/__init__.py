fc0cfc7f46b9bee51cb8b5f7fe8ee691aa39c28d
plun1331 <plun1331@gmail.com>
2024-10-24 11:28:34 -0700

    chore: update version number

--- hytek/__init__.py
+++ hytek/__init__.py
@@ -8,7 +8,7 @@ __license__ = "MIT"
 __copyright__ = "Copyright 2024-present plun13331"
 
 version_info = namedtuple("version_info", "major minor micro releaselevel serial")(
-    major=0, minor=0, micro=2, releaselevel="alpha", serial=0
+    major=0, minor=0, micro=3, releaselevel="alpha", serial=0
 )
 
 __version__ = f"{version_info.major}.{version_info.minor}.{version_info.micro}"
